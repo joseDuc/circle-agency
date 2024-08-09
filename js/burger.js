2a1b289e1a89a152d8c12d381b50720fa3cb2214
joseDuc <ppgq@live.com>
2024-08-09 10:50:57 +0200

    first

---
+++ js/burger.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', () => {
+    createMenuBurger();
+});
+
+function createMenuBurger() {
+    if (document.querySelector('#burger')) {
+        if (createMenuVertical()) {
+            const burger = document.querySelector('#burger')
+             //burger former
+            for (i = 0; i < 3; ++i) {
+                let div=document.createElement('div');
+                burger.appendChild(div);
+            }
+            burger.addEventListener('click', () => {
+                //add clic event for alternate menu
+                menuV=document.querySelector('#nav-menu-vertical').classList.toggle('active');
+            });
+        };
+    }
+}
+function createMenuVertical() {
+    //copy elements from nav-menu-Horizontal
+    if (document.querySelector('#nav-menu-horizontal')) {
+        const menuH = document.querySelector('#nav-menu-horizontal');
+        const links = menuH.querySelectorAll('.menu-link');
+        let menuV = null;
+        if (!document.querySelector('#nav-menu-vertical')) {
+            menuV = document.createElement('div');
+            menuV.id = 'nav-menu-vertical';
+        } else {
+            menuV = document.querySelector('#nav-menu-vertical');
+            menuV.innerHTML = "";
+        }
+        links.forEach(element => {
+            let a = document.createElement('a');
+            a.classList = element.classList;
+            a.innerHTML = element.innerHTML;
+            a.href=element.href;
+            menuV.appendChild(a);
+        });
+        const nav=document.querySelector('nav');
+        if (nav){
+            nav.appendChild(menuV);
+            menuV.addEventListener('click',()=>{
+                menuV.className='';
+            });
+            return true;
+        }
+    }
+}
---
+++ js/scroll.js
@@ -0,0 +1,26 @@
+
+window.addEventListener('load', () => {
+    addEventWindowMove();
+});
+
+function addEventWindowMove() {
+    if (document.querySelector('#btn-go-top')) {
+        const b = document.querySelector('#btn-go-top');
+        document.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                if (!b.classList.contains('active')){
+                    b.classList.toggle('active');
+                }
+            } else {
+                if (b.classList.contains('active')){
+                    b.classList.toggle('active');
+                }
+            }
+        });
+        b.addEventListener('click', () => {
+            window.scrollTo({
+                top:0
+            });
+        });
+    }
+}
