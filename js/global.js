9e61b8809145afdabe18e19fa1bbc9e96df42541
joseDuc <ppgq@live.com>
2024-08-12 15:03:38 +0200

    +service and newsletter

--- js/global.js
+++ js/global.js
@@ -1,5 +1,9 @@
 var rutaProjects = 'https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects'
 
+window.addEventListener('load',()=>{
+    
+    insertaFormNewsLetter();
+});
 function appendJsonToProjectArticles(data) {
     const container = document.querySelector('#projects .container');
     if (container) {
@@ -27,7 +31,7 @@ function convertToProyectAnchor(project) {
     anchor.href = `./html/projects.html?id=${project.uuid}`;
     img.className = 'project-img';
     img.src = project.image;
-    img.alt=project.image;
+    img.alt = project.image;
     anchor.appendChild(img);
     anchor.appendChild(convertToProjectText(project));
     return anchor;
@@ -50,4 +54,54 @@ function convertToProjectText(project) {
     a.href = `./html/projects.html?id=${project.uuid}`;
     div.appendChild(a);
     return div;
+}
+
+function insertaFormNewsLetter() {
+    const news = document.querySelector('#newsletter');
+    if (news) {
+        const f = document.createElement('form');
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+        const i =document.createElement('input');
+        const b=document.createElement('button')
+        const d =document.createElement('div');
+        h3.innerText = 'Do you have any questions?';
+        f.appendChild(h3);
+        p.innerText = 'Let us help you!'
+        f.appendChild(p)
+        i.type='email';
+        i.placeholder='Enter your email';
+       
+        b.type='submit';
+        b.innerText='Subscribe';
+        b.className='btn';
+        d.class='suscribe';
+        d.appendChild(i);
+        d.appendChild(b);
+        f.appendChild(d);
+        news.appendChild(f);
+        f.className='centeredAtNucleo';
+    }
+}
+
+function getQuerystring(param) {
+    //un método que he de aprender para encontrar cualquier parámetro del querystring en cualquier futura URL
+    alert('query')
+    if (param){
+        const params = new Proxy(new URLSearchParams(window.location.search), {
+            get: (searchParams, prop) => searchParams.get(prop)
+        });
+        switch (param.toLowerCase()){
+            case 'id':
+                return params.id;
+            case 'email':
+                return params.email;
+            default:
+                return null;
+        }
+    }else{
+        return null;
+    }
+    
+    
 }
